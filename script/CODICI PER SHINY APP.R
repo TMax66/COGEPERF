a227f1f9b0bf27dcee63048c0af55d517ffa5e71
TMax66 <massimo.tranquillo@gmail.com>
2021-06-10 12:07:30 +0200

    update

--- script/CODICI PER SHINY APP.R
+++ script/CODICI PER SHINY APP.R
@@ -6,6 +6,18 @@ source("librerie.R")
 tabIZSLER <- readRDS(file = here( "data", "processed", "TABELLA.rds"))
 
 
+ tabIZSLER%>% 
+  rename("ricavi" = valore, "VP" = ricavovp, "AI" = valoreai) %>% 
+  group_by(Anno, Dipartimento) %>% 
+  summarise_at(c("esami", "ricavi",  "VP", "AI", "FTED", "FTEC","costi"), sum) %>% View()
+  mutate(RT = (ricavi+VP+AI),
+         FTE_t = round((FTE_d+FTE_c),1)) %>%
+  arrange(desc(esami)) %>%
+  adorn_totals(where = "row", name = "Totale") %>%
+  mutate("R-FTE" = round(RT/FTE_t,0) ) %>%
+  select(Dipartimento, "N.esami" = esami, "FTED" = FTE_d,   "FTEC" = FTE_c, "FTET" = FTE_t, "RA" = ricavi, "RVP" = VP,
+         "RAI" = AI, "RT" = RT, "R/FTET" = "R-FTE")
+
 
 ###tabella Dipartimenti###
 
---
+++ script/Start.R
@@ -0,0 +1,1 @@
+source("PREPARAZIONE DATI.R")
