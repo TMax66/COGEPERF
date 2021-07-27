0aa2bb975830db344995f8e1129d4ee2b5a46855
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2021-07-27 15:18:06 +0200

    update

--- Centri di Costo/global.R
+++ Centri di Costo/global.R
@@ -7,13 +7,16 @@ library(DT)
 library(here)
 library(hrbrthemes)
 library(patchwork)
+library(readr)
+
+cc <- read_delim(here("data", "raw", "coge1921.txt"), 
+                 "\t", escape_double = FALSE, locale = locale(decimal_mark = ",", 
+                                                              grouping_mark = "."), trim_ws = TRUE)
+
+names(cc)[5:9] <- c("Dipartimento", "Reparto", "Laboratorio", "Centro di Costo", "CodCC")
 
 
-analisi <- read_excel(sheet = "Report 1", here(  "data", "raw",  "analisi1921.xls"))
-
-names(analisi)[1:4] <- c("Dipartimento", "Reparto", "Laboratorio", "Centro di Costo", "CodCC")
-
-dtanalisi <- analisi %>% 
+dtanalisi <- cc %>% 
   mutate(ClassAnalisi = recode(`Cod. classificazione`, 
                                `-1` = "Ufficiale a Pagamento", 
                                `-3` = "Ufficiale a Pagamento", 
@@ -25,42 +28,94 @@ dtanalisi <- analisi %>%
                                `-11` = "Ufficiale Gratuito", 
                                `-6`  = "Non Ufficiale Gratuito", 
                                `-10` = "Non Ufficiale Gratuito", 
-                               `-13` = "Non Ufficiale Gratuito" 
-  ), 
-  Pagamento = recode(`Cod. classificazione`, 
-                     `-1` = "Pagamento", 
-                     `-3` = "Pagamento", 
-                     `-8` = "Pagamento", 
-                     `-9` = "Pagamento", 
-                     `-4` = "Gratuito", 
-                     `-5` = "Gratuito", 
-                     `-7` = "Gratuito", 
-                     `-11` = "Gratuito", 
-                     `-6`  = "Gratuito", 
-                     `-10` = "Gratuito", 
-                     `-13` = "Gratuito" 
-  ), 
-  Uff = recode (`Cod. classificazione`, 
-                `-1` = "Ufficiale", 
-                `-3` = "Ufficiale", 
-                `-8` = "Non Ufficiale", 
-                `-9` = "Non Ufficiale", 
-                `-4` = "Ufficiale", 
-                `-5` = "Ufficiale", 
-                `-7` = "Ufficiale", 
-                `-11` = "Ufficiale", 
-                `-6`  = "Non Ufficiale", 
-                `-10` = "Non Ufficiale", 
-                `-13` = "Non Ufficiale" ), 
-  
-  
-  Quarter = factor(paste(`N. Trimestre`,"-", "Trim")), 
-  TUff = ifelse(ClassAnalisi == "Ufficiale a Pagamento", Fatturato, 
-                       ifelse(ClassAnalisi == "Ufficiale Gratuito", `A Tariffario`, 0)), 
-  TNonUff = ifelse(ClassAnalisi == "Non Ufficiale a Pagamento", Fatturato, 
-                          ifelse(ClassAnalisi == "Non Ufficiale Gratuito", `A Tariffario`, 0)), 
-  TGratuito = ifelse(Pagamento == "Pagamento", Fatturato, 
-                     ifelse(Pagamento == "Gratuito",`A Tariffario`, 0 )))  
+                               `-13` = "Non Ufficiale Gratuito" ,  .default = NA_character_),
+         Pagamento = recode(`Cod. classificazione`, 
+                            `-1` = "Pagamento", 
+                            `-3` = "Pagamento", 
+                            `-8` = "Pagamento", 
+                            `-9` = "Pagamento", 
+                            `-4` = "Gratuito", 
+                            `-5` = "Gratuito", 
+                            `-7` = "Gratuito", 
+                            `-11` = "Gratuito", 
+                            `-6`  = "Gratuito", 
+                            `-10` = "Gratuito", 
+                            `-13` = "Gratuito" ,  .default = NA_character_), 
+         Uff = recode (`Cod. classificazione`, 
+                       `-1` = "Ufficiale", 
+                       `-3` = "Ufficiale", 
+                       `-8` = "Non Ufficiale", 
+                       `-9` = "Non Ufficiale", 
+                       `-4` = "Ufficiale", 
+                       `-5` = "Ufficiale", 
+                       `-7` = "Ufficiale", 
+                       `-11` = "Ufficiale", 
+                       `-6`  = "Non Ufficiale", 
+                       `-10` = "Non Ufficiale", 
+                       `-13` = "Non Ufficiale", .default = NA_character_), 
+         
+         Quarter = factor(paste(`N. Trimestre`,"-", "Trim")),
+         TUff = ifelse(ClassAnalisi == "Ufficiale a Pagamento", Fatturato,
+                       ifelse(ClassAnalisi == "Ufficiale Gratuito", `A Tariffario`, 0)),
+         TNonUff = ifelse(ClassAnalisi == "Non Ufficiale a Pagamento", Fatturato,
+                          ifelse(ClassAnalisi == "Non Ufficiale Gratuito", `A Tariffario`, 0)),
+         TGratuito = ifelse(Pagamento == "Pagamento", Fatturato,
+                            ifelse(Pagamento == "Gratuito",`A Tariffario`, 0 )))
+
+
+
+# analisi <- read_excel(sheet = "Report 1", here(  "data", "raw",  "analisi1921.xls"))
+# 
+# names(analisi)[1:4] <- c("Dipartimento", "Reparto", "Laboratorio", "Centro di Costo", "CodCC")
+# 
+# dtanalisi <- analisi %>% 
+#   mutate(ClassAnalisi = recode(`Cod. classificazione`, 
+#                                `-1` = "Ufficiale a Pagamento", 
+#                                `-3` = "Ufficiale a Pagamento", 
+#                                `-8` = "Non Ufficiale a Pagamento", 
+#                                `-9` = "Non Ufficiale a Pagamento", 
+#                                `-4` = "Ufficiale Gratuito", 
+#                                `-5` = "Ufficiale Gratuito", 
+#                                `-7` = "Ufficiale Gratuito", 
+#                                `-11` = "Ufficiale Gratuito", 
+#                                `-6`  = "Non Ufficiale Gratuito", 
+#                                `-10` = "Non Ufficiale Gratuito", 
+#                                `-13` = "Non Ufficiale Gratuito" 
+#   ), 
+#   Pagamento = recode(`Cod. classificazione`, 
+#                      `-1` = "Pagamento", 
+#                      `-3` = "Pagamento", 
+#                      `-8` = "Pagamento", 
+#                      `-9` = "Pagamento", 
+#                      `-4` = "Gratuito", 
+#                      `-5` = "Gratuito", 
+#                      `-7` = "Gratuito", 
+#                      `-11` = "Gratuito", 
+#                      `-6`  = "Gratuito", 
+#                      `-10` = "Gratuito", 
+#                      `-13` = "Gratuito" 
+#   ), 
+#   Uff = recode (`Cod. classificazione`, 
+#                 `-1` = "Ufficiale", 
+#                 `-3` = "Ufficiale", 
+#                 `-8` = "Non Ufficiale", 
+#                 `-9` = "Non Ufficiale", 
+#                 `-4` = "Ufficiale", 
+#                 `-5` = "Ufficiale", 
+#                 `-7` = "Ufficiale", 
+#                 `-11` = "Ufficiale", 
+#                 `-6`  = "Non Ufficiale", 
+#                 `-10` = "Non Ufficiale", 
+#                 `-13` = "Non Ufficiale" ), 
+#   
+#   
+#   Quarter = factor(paste(`N. Trimestre`,"-", "Trim")), 
+#   TUff = ifelse(ClassAnalisi == "Ufficiale a Pagamento", Fatturato, 
+#                        ifelse(ClassAnalisi == "Ufficiale Gratuito", `A Tariffario`, 0)), 
+#   TNonUff = ifelse(ClassAnalisi == "Non Ufficiale a Pagamento", Fatturato, 
+#                           ifelse(ClassAnalisi == "Non Ufficiale Gratuito", `A Tariffario`, 0)), 
+#   TGratuito = ifelse(Pagamento == "Pagamento", Fatturato, 
+#                      ifelse(Pagamento == "Gratuito",`A Tariffario`, 0 )))  
 
 #funzioni----
 
--- Centri di Costo/script/onthefly.R
+++ Centri di Costo/script/onthefly.R
@@ -1,5 +1,83 @@
+library(shiny)
+library(tidyverse)
+library(readxl)
+library(lubridate)
+library(shinythemes)
+library(DT)
+library(here)
+library(hrbrthemes)
+library(patchwork)
 
-dtanalisi %>% 
+library(readr)
+cc <- read_delim("data/raw/coge1921.txt", 
+                       "\t", escape_double = FALSE, trim_ws = TRUE)
+
+names(cc)[5:9] <- c("Dipartimento", "Reparto", "Laboratorio", "Centro di Costo", "CodCC")
+ 
+
+dtanalisi <- cc %>% 
+  mutate(ClassAnalisi = recode(`Cod. classificazione`, 
+                               `-1` = "Ufficiale a Pagamento", 
+                               `-3` = "Ufficiale a Pagamento", 
+                               `-8` = "Non Ufficiale a Pagamento", 
+                               `-9` = "Non Ufficiale a Pagamento", 
+                               `-4` = "Ufficiale Gratuito", 
+                               `-5` = "Ufficiale Gratuito", 
+                               `-7` = "Ufficiale Gratuito", 
+                               `-11` = "Ufficiale Gratuito", 
+                               `-6`  = "Non Ufficiale Gratuito", 
+                               `-10` = "Non Ufficiale Gratuito", 
+                               `-13` = "Non Ufficiale Gratuito" ,  .default = NA_character_),
+         Pagamento = recode(`Cod. classificazione`, 
+                     `-1` = "Pagamento", 
+                     `-3` = "Pagamento", 
+                     `-8` = "Pagamento", 
+                     `-9` = "Pagamento", 
+                     `-4` = "Gratuito", 
+                     `-5` = "Gratuito", 
+                     `-7` = "Gratuito", 
+                     `-11` = "Gratuito", 
+                     `-6`  = "Gratuito", 
+                     `-10` = "Gratuito", 
+                     `-13` = "Gratuito" ,  .default = NA_character_), 
+         Uff = recode (`Cod. classificazione`, 
+                `-1` = "Ufficiale", 
+                `-3` = "Ufficiale", 
+                `-8` = "Non Ufficiale", 
+                `-9` = "Non Ufficiale", 
+                `-4` = "Ufficiale", 
+                `-5` = "Ufficiale", 
+                `-7` = "Ufficiale", 
+                `-11` = "Ufficiale", 
+                `-6`  = "Non Ufficiale", 
+                `-10` = "Non Ufficiale", 
+                `-13` = "Non Ufficiale", .default = NA_character_), 
+        
+        Quarter = factor(paste(`N. Trimestre`,"-", "Trim")),
+  TUff = ifelse(ClassAnalisi == "Ufficiale a Pagamento", Fatturato,
+                ifelse(ClassAnalisi == "Ufficiale Gratuito", `A Tariffario`, 0)),
+  TNonUff = ifelse(ClassAnalisi == "Non Ufficiale a Pagamento", Fatturato,
+                   ifelse(ClassAnalisi == "Non Ufficiale Gratuito", `A Tariffario`, 0)),
+  TGratuito = ifelse(Pagamento == "Pagamento", Fatturato,
+                     ifelse(Pagamento == "Gratuito",`A Tariffario`, 0 )))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cc %>% 
   mutate(TUff = ifelse(ClassAnalisi == "Ufficiale a Pagamento", Fatturato, 
                        ifelse(ClassAnalisi == "Ufficiale Gratuito", `A Tariffario`, 0)), 
          TNonUff = ifelse(ClassAnalisi == "Non Ufficiale a Pagamento", Fatturato, 
@@ -57,13 +135,14 @@ dtanalisi %>%
 
 
 dt <- dtanalisi %>% 
+                   filter(`Costo o Ricavo`=="Ricavo") %>%  
   group_by(`Centro di Costo`, Pagamento, ClassAnalisi,Uff, Anno,  Quarter) %>% 
-  summarise(Valorizzato = round(sum(`A Tariffario`, na.rm = TRUE), 0), 
-            Fatturato = round(sum(Fatturato, na.rm = TRUE), 0)) %>% 
-  mutate(VarVal = round((Valorizzato/lag(Valorizzato) - 1) * 100, 2 ), 
-         VarFatt= round((Fatturato/lag(Fatturato)-1)*100, 2)) %>% 
-  filter(`Centro di Costo` == "SEDE TERRITORIALE DI BERGAMO") %>% 
-  pivot_longer(cols = 7:10, names_to = "Parametro", values_to = "metrica") %>% 
+                   summarise(Tariffato = round(sum(`A Tariffario`, na.rm = TRUE), 0), 
+                             Fatturato = round(sum(Fatturato, na.rm = TRUE), 0)) %>% 
+                   mutate(VarVal = round((Tariffato/lag(Tariffato) - 1) * 100, 2 ), 
+                          VarFatt= round((Fatturato/lag(Fatturato)-1)*100, 2)) %>% 
+  filter(`Centro di Costo` == "SEDE TERRITORIALE DI BERGAMO") %>%
+  pivot_longer(cols = 7:10, names_to = "Parametro", values_to = "metrica") %>%  View()
   to_be(Pagamento = "Pagamento") %>%  
   filter(Pagamento == "Pagamento" & Uff == "Ufficiale") %>%  
   pivot_wider(names_from = "Parametro", values_from = "metrica") %>% 
