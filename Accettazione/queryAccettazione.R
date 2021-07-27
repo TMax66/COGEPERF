9924363dd8f56d27a3fde7644eaeb91d6295cab1
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2021-07-27 11:41:57 +0200

    update

--- Accettazione/queryAccettazione.R
+++ Accettazione/queryAccettazione.R
@@ -1,69 +1,50 @@
 
 
-
-
-
-
-
-
-
-
-
-
 #importazione accettazione----- da query esportata da SQLServer ( vedi sotto)
 
 library(readr)
-acc <- read_delim("C:/Users/vito.tranquillo/Desktop/prova.txt", 
-           "\t", escape_double = FALSE, col_names = FALSE, 
-           trim_ws = TRUE)
+acc <- read_delim("Accettazione/postazioni.txt", 
+                         "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE)
 
+names(acc) <- c("nconf", "strpropr", "settore", "finalità", "pagamento", 
+                "dtprel", "dtreg", "dtacc", "dtrdp", "IstRDP",  "pc", "gruppoprova")
 
-names(acc) <- c("nconf", "stracc", "strpropr", "utente")
+View(acc)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# query <- "SELECT
-#   dbo.Conferimenti.Numero,
-#   dbo_Anag_Reparti_ConfAcc.Descrizione,
-#   dbo_Anag_Reparti_ConfProp.Descrizione,
-#   dbo_Operatori_di_sistema_ConfMatr.Descr_Completa,
-#   dbo.Anag_Gruppo_Prove.Descrizione,
-#   dbo.Conferimenti.Data_Prelievo,
-#   dbo.Conferimenti.Data,
-#   dbo.Conferimenti.Data_Inserimento,
-#   dbo_Giorni_Lavorativi_DataConf.Nr_del_giorno
+# SELECT
+# dbo.Conferimenti.Numero,
+# dbo_Anag_Reparti_ConfProp.Descrizione,
+# dbo.Anag_Registri.Descrizione,
+# dbo_Anag_Finalita_Confer.Descrizione,
+# dbo.Anag_TipoConf.Descrizione,
+# dbo.Conferimenti.Data_Prelievo,
+# dbo.Conferimenti.Data,
+# dbo.Conferimenti.Data_Accettazione,
+# dbo.Conferimenti.DataOra_Primo_RDP_Completo_Firmato,
+# dbo.RDP_Date_Emissione.Istanza_RDP,
+# dbo.Conferimenti.Nome_Stazione_Inserimento,
+# dbo.Anag_Gruppo_Prove.Descrizione
 # FROM
-# { oj dbo.Anag_Reparti  dbo_Anag_Reparti_ConfAcc INNER JOIN dbo.Laboratori_Reparto  dbo_Laboratori_Reparto_ConfAcc ON ( dbo_Laboratori_Reparto_ConfAcc.Reparto=dbo_Anag_Reparti_ConfAcc.Codice )
-#    INNER JOIN dbo.Conferimenti ON ( dbo.Conferimenti.RepLab_Conferente=dbo_Laboratori_Reparto_ConfAcc.Chiave )
-#    LEFT OUTER JOIN dbo.Esami_Aggregati ON ( dbo.Conferimenti.Anno=dbo.Esami_Aggregati.Anno_Conferimento and dbo.Conferimenti.Numero=dbo.Esami_Aggregati.Numero_Conferimento )
-#    LEFT OUTER JOIN dbo.Nomenclatore_MP ON ( dbo.Esami_Aggregati.Nomenclatore=dbo.Nomenclatore_MP.Codice )
-#    LEFT OUTER JOIN dbo.Nomenclatore_Settori ON ( dbo.Nomenclatore_MP.Nomenclatore_Settore=dbo.Nomenclatore_Settori.Codice )
-#    LEFT OUTER JOIN dbo.Nomenclatore ON ( dbo.Nomenclatore_Settori.Codice_Nomenclatore=dbo.Nomenclatore.Chiave )
-#    LEFT OUTER JOIN dbo.Anag_Gruppo_Prove ON ( dbo.Nomenclatore.Codice_Gruppo=dbo.Anag_Gruppo_Prove.Codice )
-#    INNER JOIN dbo.Laboratori_Reparto  dbo_Laboratori_Reparto_ConfProp ON ( dbo.Conferimenti.RepLab=dbo_Laboratori_Reparto_ConfProp.Chiave )
-#    INNER JOIN dbo.Anag_Reparti  dbo_Anag_Reparti_ConfProp ON ( dbo_Laboratori_Reparto_ConfProp.Reparto=dbo_Anag_Reparti_ConfProp.Codice )
-#    INNER JOIN dbo.Operatori_di_sistema  dbo_Operatori_di_sistema_ConfMatr ON ( dbo.Conferimenti.Matr_Ins=dbo_Operatori_di_sistema_ConfMatr.Ident_Operatore )
-#    INNER JOIN dbo.Giorni_Lavorativi  dbo_Giorni_Lavorativi_DataConf ON ( dbo_Giorni_Lavorativi_DataConf.Giorno=dbo.Conferimenti.Data )
-#   }
+# { oj dbo.Anag_Reparti  dbo_Anag_Reparti_ConfProp INNER JOIN dbo.Laboratori_Reparto  dbo_Laboratori_Reparto_ConfProp ON ( dbo_Laboratori_Reparto_ConfProp.Reparto=dbo_Anag_Reparti_ConfProp.Codice )
+#   INNER JOIN dbo.Conferimenti ON ( dbo.Conferimenti.RepLab=dbo_Laboratori_Reparto_ConfProp.Chiave )
+#   LEFT OUTER JOIN dbo.Esami_Aggregati ON ( dbo.Conferimenti.Anno=dbo.Esami_Aggregati.Anno_Conferimento and dbo.Conferimenti.Numero=dbo.Esami_Aggregati.Numero_Conferimento )
+#   LEFT OUTER JOIN dbo.Nomenclatore_MP ON ( dbo.Esami_Aggregati.Nomenclatore=dbo.Nomenclatore_MP.Codice )
+#   LEFT OUTER JOIN dbo.Nomenclatore_Settori ON ( dbo.Nomenclatore_MP.Nomenclatore_Settore=dbo.Nomenclatore_Settori.Codice )
+#   LEFT OUTER JOIN dbo.Nomenclatore ON ( dbo.Nomenclatore_Settori.Codice_Nomenclatore=dbo.Nomenclatore.Chiave )
+#   LEFT OUTER JOIN dbo.Anag_Gruppo_Prove ON ( dbo.Nomenclatore.Codice_Gruppo=dbo.Anag_Gruppo_Prove.Codice )
+#   INNER JOIN dbo.Anag_Registri ON ( dbo.Conferimenti.Registro=dbo.Anag_Registri.Codice )
+#   INNER JOIN dbo.Anag_TipoConf ON ( dbo.Anag_TipoConf.Codice=dbo.Conferimenti.Tipo )
+#   INNER JOIN dbo.Conferimenti_Finalita ON ( dbo.Conferimenti.Anno=dbo.Conferimenti_Finalita.Anno and dbo.Conferimenti.Numero=dbo.Conferimenti_Finalita.Numero )
+#   INNER JOIN dbo.Anag_Finalita  dbo_Anag_Finalita_Confer ON ( dbo.Conferimenti_Finalita.Finalita=dbo_Anag_Finalita_Confer.Codice )
+#   LEFT OUTER JOIN dbo.RDP_Date_Emissione ON ( dbo.RDP_Date_Emissione.Anno=dbo.Conferimenti.Anno and dbo.RDP_Date_Emissione.Numero=dbo.Conferimenti.Numero )
+# }
 # WHERE
-#   dbo.Esami_Aggregati.Esame_Altro_Ente = 0
-#   AND  (
-#   {fn year(dbo.Conferimenti.Data_Accettazione)}  =  2021
-#   )
-# "
+# dbo.Esami_Aggregati.Esame_Altro_Ente = 0
+# AND  (
+#   {fn year(dbo.Conferimenti.Data_Accettazione)}  >=  2019  )
+# 
+# AND  dbo.Conferimenti.Nome_Stazione_Inserimento  IN  ('ACC-CENTR2', 'PC-47326', 'PC-40780', 'MP-ACC3', 'BS-ASS-N', 'PC-47327', 'CH-ACC4-N', 'CH-ACC2-N', 'MP-SIVARS7', 'PC-47499', 'PC-47929', 'OEVR-8-PORT'
+# )
+
+
+
