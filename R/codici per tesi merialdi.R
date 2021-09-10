340ea8b0e517938631561981c90b89a43a5ce3b4
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2021-09-10 12:24:10 +0200

    update

--- R/codici per tesi merialdi.R
+++ R/codici per tesi merialdi.R
@@ -10,86 +10,167 @@ prj <- read_excel(sheet = "PRJ", here("data", "raw", "prj2020.xlsx"))
 
 
 
-dtf1 <- c( "2017-01-01","2018-01-01","2019-01-01","2020-01-01", "2021-01-01")
+dt1 <- c(  "2018-01-01","2019-01-01","2020-01-01"  )
 
-dti <-  c("2017-12-31","2018-12-31","2019-12-31","2020-12-31","2021-12-31" )
-anno <- seq(from = 2017, to = 2021, by=1)
+dt2 <-  c( "2018-12-31","2019-12-31","2020-12-31" )
+anno <- seq(from = 2018, to = 2020, by=1)
 
-x <- data.frame(dtf1, dti, anno)
+x <- data.frame(dt1, dt2, anno)
+
+##funzione per progetti in corso
+# prj_func <- function( dt1, dt2, anno)
+# { prj %>%
+#     filter(Tipologia!= "Istituzionale") %>% 
+#     group_by( Tipologia) %>% 
+#     mutate("Stato" = ifelse(DataFine < as.Date(dt1), "Archiviato", "Attivo")) %>% 
+#     filter(Stato == "Attivo" & DataInizio <= as.Date(dt2)) %>%
+#     # mutate("Statoanno" = ifelse(DataFine <=as.Date(dt1), "Concluso", "Aperto")) %>%
+#     # filter(Statoanno == "Aperto") %>% 
+#     summarise( 
+#       "N.Progetti"=nlevels(factor(Codice)), 
+#       Budget = sum(Budget, na.rm = T)) %>% 
+#     mutate(anno = anno)
+#   
+# }
 
 
-prj_func <- function( dtf1, dti, anno)
+prj_func2 <- function(dt1, dt2,  anno)
 { prj %>%
+    filter(Tipologia!= "Istituzionale") %>% 
+    filter(DataInizio >= as.Date(dt1) & DataInizio <=as.Date(dt2)) %>% 
     group_by( Tipologia) %>% 
-    mutate("Stato" = ifelse(DataFine < as.Date(dtf1), "Archiviato", "Attivo")) %>% 
-    filter(Stato == "Attivo" & DataInizio <= as.Date(dti)) %>%
-    # mutate("Statoanno" = ifelse(DataFine <=as.Date(dtf1), "Concluso", "Aperto")) %>%
-    # filter(Statoanno == "Aperto") %>% 
-    summarise( 
-      # "N.Progetti"=nlevels(factor(Codice)), 
-      Budget = sum(Budget, na.rm = T)) %>% 
+    summarise(Bdg = sum(Budget), 
+              "N.Progetti"=nlevels(factor(Codice)))%>% 
     mutate(anno = anno)
   
 }
 
 
+
+
+
 z <- list()
 
-for (i in 1:5) { 
-  z[[i]]<- prj_func(  dtf1 = x[i, 1], dti = x[i, 2], anno = x[i, 3])
+for (i in 1:3) { 
+  z[[i]]<- prj_func2(  dt1 = x[i, 1], dt2 = x[i, 2], anno = x[i, 3])
   
 }
 progetti <- do.call(rbind, z)
 
-progetti %>% 
-  # pivot_wider(names_from = "Tipologia", values_from = "N.Progetti") %>% 
-  pivot_wider(names_from = "Tipologia", values_from = "Budget") %>%  
-  select(anno, Corrente, Finalizzato, Europeo, Regionali, Istituzionale, Autofinanziato, CCM, "Altro tipo" ) %>% 
-  rowwise() %>% 
-  #mutate(Competitivi = sum(Finalizzato, Europeo, Regionali, Istituzionale, CCM,`Altro tipo`)) %>% 
-  mutate(Competitivi = sum(Finalizzato, Europeo, Regionali, Istituzionale,  CCM,`Altro tipo`, na.rm = T)) %>% 
-  select(Anno = anno, "Ricerca Corrente" = Corrente, "Ricerca Competitiva" = Competitivi) %>% 
-  knitr::kable(digits = 2, caption = "Progetti in corso") %>%
-  kable_styling()  
-
-
-
-dt1 <- c( "2017-01-01","2018-01-01","2019-01-01","2020-01-01", "2021-01-01")
-
-dt2 <-  c("2017-12-31","2018-12-31","2019-12-31","2020-12-31","2021-12-31" )
-anno <- seq(from = 2017, to = 2021, by=1)
-
-x <- data.frame(dt1, dt2, anno)
-
-prj_func2 <- function(  dt1, dt2, anno)
-{  prj %>% 
-    group_by( Tipologia) %>% 
-    filter(DataInizio >= as.Date(dt1) & DataInizio <=as.Date(dt2)) %>% 
-    summarise( 
-      # "N.Progetti"=nlevels(factor(Codice)), 
-      Budget = sum(Budget, na.rm = T)) %>% 
-    mutate(anno = anno) 
+progetti <- progetti %>%
   
-}
-
-z <- list()
-
-for (i in 1:5) { 
-  z[[i]]<- prj_func2(  dt1 = x[i, 1], dt2 = x[i, 2], anno = x[i, 3])
+mutate(Ricerca = ifelse(Tipologia == "Corrente", "Non Competitiva",
+                                                   ifelse(Tipologia == "Autofinanziato", "Non Competitiva",
+                                                          "Competitiva"))) %>%
+select("Anno" = anno, Ricerca,   Tipologia, N.Progetti , "Budget" = Bdg)  %>% 
+  arrange(Anno, Ricerca)
   
-}
-nprogetti <- do.call(rbind, z)
 
 
-nprogetti %>% 
-  # pivot_wider(names_from = "Tipologia", values_from = "N.Progetti") %>% 
-  pivot_wider(names_from = "Tipologia", values_from = "Budget") %>%  
-  select(anno, Corrente, Finalizzato, Europeo, Regionali, Istituzionale, Autofinanziato, CCM, "Altro tipo" ) %>% 
-  rowwise() %>% 
-  #mutate(Competitivi = sum(Finalizzato, Europeo, Regionali, Istituzionale, CCM,`Altro tipo`)) %>% 
-  mutate(Competitivi = sum(Finalizzato, Europeo, Regionali, Istituzionale, CCM,`Altro tipo`, na.rm = T)) %>% 
-  select(Anno = anno, "Ricerca Corrente" = Corrente, "Ricerca Competitiva" = Competitivi) %>% 
-  knitr::kable(digits = 2, caption = "Nuovi Progetti") %>%
-  kable_styling()  
+
+reactable(
+  progetti,
+  columns = list(
+   Anno = colDef(
+      style = JS("function(rowInfo, colInfo, state) {
+        var firstSorted = state.sorted[0]
+        // Merge cells if unsorted or sorting by school
+        if (!firstSorted || firstSorted.id === 'Anno') {
+          var prevRow = state.pageRows[rowInfo.viewIndex - 1]
+          if (prevRow && rowInfo.row['Anno'] === prevRow['Anno']) {
+            return { visibility: 'hidden' }
+          }
+        }
+      }")
+    ), 
+   Ricerca = colDef(
+     style = JS("function(rowInfo, colInfo, state) {
+        var firstSorted = state.sorted[0]
+        // Merge cells if unsorted or sorting by school
+        if (!firstSorted || firstSorted.id === 'Ricerca') {
+          var prevRow = state.pageRows[rowInfo.viewIndex - 1]
+          if (prevRow && rowInfo.row['Ricerca'] === prevRow['Ricerca']) {
+            return { visibility: 'hidden' }
+          }
+        }
+      }")
+   )
+  ),
+  outlined = FALSE
+)
  
 
+# pivot_wider(names_from = "Tipologia", values_from = c("N.Progetti", "Budget"), values_fill = 0) %>%)
+  # rename(N.Progetti)
+  
+         
+#library(gt)
+
+
+kbl(progetti, align = "c") %>% 
+  kable_paper(full_width = F) %>% 
+  column_spec(1:2, bold = T) %>%
+  collapse_rows(1, valign = "top") %>%  
+  kable_styling()
+
+ 
+
+
+
+         
+         
+#   # pivot_wider(names_from = "Tipologia", values_from = "N.Progetti") %>% 
+#   #pivot_wider(names_from = "Tipologia", values_from = "Budget") %>%  
+#  # select(anno, Corrente, Finalizzato, Europeo, Regionali, Autofinanziato, CCM, "Altro tipo" ) %>% 
+#   
+#   
+#   
+#   
+#   
+#   
+#   
+#   
+#   knitr::kable(digits = 2, caption = "Progetti in corso") %>%
+#   kable_styling()  
+# 
+# 
+# 
+# dt1 <- c( "2017-01-01","2018-01-01","2019-01-01","2020-01-01", "2021-01-01")
+# 
+# dt2 <-  c("2017-12-31","2018-12-31","2019-12-31","2020-12-31","2021-12-31" )
+# anno <- seq(from = 2017, to = 2021, by=1)
+# 
+# x <- data.frame(dt1, dt2, anno)
+# 
+# prj_func2 <- function(  dt1, dt2, anno)
+# {  prj %>% 
+#     group_by( Tipologia) %>% 
+#     filter(DataInizio >= as.Date(dt1) & DataInizio <=as.Date(dt2)) %>% 
+#     summarise( 
+#       # "N.Progetti"=nlevels(factor(Codice)), 
+#       Budget = sum(Budget, na.rm = T)) %>% 
+#     mutate(anno = anno) 
+#   
+# }
+# 
+# z <- list()
+# 
+# for (i in 1:5) { 
+#   z[[i]]<- prj_func2(  dt1 = x[i, 1], dt2 = x[i, 2], anno = x[i, 3])
+#   
+# }
+# nprogetti <- do.call(rbind, z)
+# 
+# 
+# nprogetti %>% 
+#   # pivot_wider(names_from = "Tipologia", values_from = "N.Progetti") %>% 
+#   pivot_wider(names_from = "Tipologia", values_from = "Budget") %>%  
+#   select(anno, Corrente, Finalizzato, Europeo, Regionali, Istituzionale, Autofinanziato, CCM, "Altro tipo" ) %>% 
+#   rowwise() %>% 
+#   #mutate(Competitivi = sum(Finalizzato, Europeo, Regionali, Istituzionale, CCM,`Altro tipo`)) %>% 
+#   mutate(Competitivi = sum(Finalizzato, Europeo, Regionali, Istituzionale, CCM,`Altro tipo`, na.rm = T)) %>% 
+#   select(Anno = anno, "Ricerca Corrente" = Corrente, "Ricerca Competitiva" = Competitivi) %>% 
+#   knitr::kable(digits = 2, caption = "Nuovi Progetti") %>%
+#   kable_styling()  
+#  
+# 
