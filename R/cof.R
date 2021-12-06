bcb62134b8095562de0f3fa974a7fc25fc72f9c3
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2021-12-06 14:51:30 +0100

    update

--- R/cof.R
+++ R/cof.R
@@ -11,3 +11,35 @@ tizsler %>% filter(Anno == 2021) %>% View()
          summarise("Pubblicazioni" = nlevels(factor(NR)), 
                    "Impact Factor" = sum(IF, na.rm = TRUE), 
                    "IF mediano" = median(IF, na.rm = TRUE))), by = "Dipartimento") 
+    
+    
+    
+    fte <- ore %>% 
+      mutate(Dirigente = recode(Dirigente, N = "Comparto", S = "Dirigenza")) %>% 
+      filter(Dipartimento != "Non applicabile" &
+             !is.na(Dirigente) & 
+               !is.na(Ore) & 
+               Mese == 12 & 
+               ANNO == 2020) %>%
+      group_by(ANNO, Dipartimento, Reparto, Laboratorio, Dirigente) %>%
+      mutate(fte = ifelse(Dirigente == "Comparto",  36*47.4,  38*47.4)) %>%  
+      summarise(FTE = sum(fte)/fte) %>% 
+      distinct() %>% 
+      pivot_wider(names_from = "Dirigente", values_from = "FTE", names_repair = c("Dirigente", "FTE"))  
+       
+      
+      
+      
+      
+      
+      
+      fte <- ore %>% 
+        mutate(Dirigente = recode(Dirigente, N = "Comparto", S = "Dirigenza")) %>% 
+        filter(Dipartimento != "Non applicabile") %>% 
+        group_by(ANNO, Dipartimento, Reparto, Laboratorio, Dirigente) %>%   
+        filter(!is.na(Dirigente) & !is.na(Ore)) %>% 
+        summarise(hworked = sum(Ore, na.rm = T)) %>% 
+        mutate(FTE = ifelse(Dirigente == "Comparto", hworked/(36*47.4), hworked/(38*47.4))) %>% 
+        pivot_wider(names_from = "Dirigente", values_from = c("hworked", "FTE"))  %>% 
+        select(-hworked_, -FTE_)  
+      
