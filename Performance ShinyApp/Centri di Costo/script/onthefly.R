de6f7934c40459bf099e4bffd664b9ff047a5ba7
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2022-01-14 15:29:19 +0100

    update

--- Performance ShinyApp/Centri di Costo/script/onthefly.R
+++ Performance ShinyApp/Centri di Costo/script/onthefly.R
@@ -1,3 +1,145 @@
+dtanalisi %>%  
+  filter(Costi== "Ricavo") %>% 
+  group_by(ANNO, Quarter, Dipartimento, Reparto, Laboratorio, CDC, ClassAnalisi, Classe, Area) %>% 
+  filter(Classe %in% c("Prestazioni", "Vendite prodotti", "Ricavi da produzione")) %>%  
+  summarise(N_Det = sum(Determinazioni, na.rm = TRUE),
+            N_Num = sum(Numero, na.rm = TRUE), 
+            S_Tariffa = sum(Tariffario, na.rm = TRUE), 
+            S_Fatturato = sum(Fatturato, na.rm = TRUE))  %>% 
+  filter(CDC == "SEDE TERRITORIALE DI BERGAMO" & Classe == "Prestazioni") %>% 
+  group_by(ANNO, Quarter, Area) %>% 
+  summarise(N = sum(N_Det, na.rm=TRUE)) %>% 
+  mutate(YQ = paste(ANNO, "-", Quarter)) %>% ungroup() %>% 
+  select(-ANNO, -Quarter) %>% 
+  arrange(Area, YQ) %>% 
+  mutate(CumN = cumsum(N)) %>%  View()
+  pivot_wider( names_from = YQ,  values_from = N, values_fill = 0) %>%  View()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 dtAtt <-  dtanalisi %>% filter(CDC == "SEDE TERRITORIALE DI BERGAMO" & Costi=="Ricavo") %>% 
                      group_by(CDC,  ANNO,  Quarter) %>% 
                      summarise(N.Esami = sum(Determinazioni, na.rm = TRUE), 
