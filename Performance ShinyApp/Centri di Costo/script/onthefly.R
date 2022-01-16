4c8432c9c820547a9abe65e92f1308b417dd3b34
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2022-01-16 11:19:49 +0100

    update

--- Performance ShinyApp/Centri di Costo/script/onthefly.R
+++ Performance ShinyApp/Centri di Costo/script/onthefly.R
@@ -6,15 +6,39 @@ dtanalisi %>%
             N_Num = sum(Numero, na.rm = TRUE), 
             S_Tariffa = sum(Tariffario, na.rm = TRUE), 
             S_Fatturato = sum(Fatturato, na.rm = TRUE))  %>% 
-  filter(CDC == "SEDE TERRITORIALE DI BERGAMO" & Classe == "Prestazioni") %>% 
+  filter(CDC == input$CC & Classe == "Prestazioni") %>% 
   group_by(ANNO, Quarter, Area) %>% 
   summarise(N = sum(N_Det, na.rm=TRUE)) %>% 
   mutate(YQ = paste(ANNO, "-", Quarter)) %>% ungroup() %>% 
   select(-ANNO, -Quarter) %>% 
-  arrange(Area, YQ) %>% 
-  mutate(CumN = cumsum(N)) %>%  View()
-  pivot_wider( names_from = YQ,  values_from = N, values_fill = 0) %>%  View()
-
+  pivot_wider( names_from = YQ,  values_from = N, values_fill = 0) %>%   
+  #rename(., "Prestazione" = Area) %>% 
+  left_join(  
+    
+    (dtanalisi %>% 
+       filter(Costi == "Ricavo") %>% 
+       group_by(ANNO, Quarter, Dipartimento, Reparto, Laboratorio, CDC,ClassAnalisi, Classe, Area) %>% 
+       filter(Classe %in% c("Prestazioni", "Vendite prodotti", "Ricavi da produzione")) %>%  
+       summarise(N_Det = sum(Determinazioni, na.rm = TRUE),
+                 N_Num = sum(Numero, na.rm = TRUE), 
+                 S_Tariffa = sum(Tariffario, na.rm = TRUE), 
+                 S_Fatturato = sum(Fatturato, na.rm = TRUE))  %>% 
+       filter(CDC == input$CC & Classe == "Prestazioni") %>% 
+       group_by(ANNO, Quarter, Area) %>% 
+       summarise(N = sum(N_Det, na.rm=TRUE)) %>% 
+       mutate(YQ = paste(ANNO, "-", Quarter)) %>%
+       select(-ANNO, -Quarter) %>% 
+       group_by(Area) %>%
+       summarise(trend = spk_chr(N, type= "line", options =
+                                   list(paging = FALSE)))
+    )) %>% rename("Prestazioni" = Area) %>% 
+  
+  format_table()  %>% 
+  htmltools::HTML() %>% 
+  div() %>% 
+  spk_add_deps()
+ 
+ 
 
 
 
