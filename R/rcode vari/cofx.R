f0fbe9813572e547a847af7931572f689c9777a0
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2022-02-14 14:47:24 +0100

    update

--- R/rcode vari/cofx.R
+++ R/rcode vari/cofx.R
@@ -4,6 +4,7 @@ library(here)
  
 ore <- readRDS(here("data", "processed", "ore.RDS"))
 ricavi <- readRDS(here("data", "processed", "CC.rds"))
+ftep <- readRDS(here("data", "processed", "ftepDIP.RDS"))
 
 
 #fteq mensili
@@ -18,13 +19,44 @@ ore %>%
   mutate(FTE = ifelse(Dirigente == "Comparto", hworked/(36*4.34), hworked/(38*4.34))) %>% 
   pivot_wider(names_from = "Dirigente", values_from = c("hworked", "FTE"))  %>% 
   select(-hworked_, -FTE_)  %>% 
-  mutate(FTET = FTE_Comparto+FTE_Dirigenza)
+  mutate(FTET = FTE_Comparto+FTE_Dirigenza) %>%  
+  group_by(Mese, Dipartimento) %>% 
+  summarise(FTET = sum(FTET, na.rm=TRUE)) %>%  
+  filter(!Dipartimento %in% c("Costi Comuni e Centri contabili", 
+                              "Dipartimento amministrativo",
+                              "Direzione Amministrativa",
+                              "Direzione Generale")) %>% 
+  
+  left_join(  
 
 
 
 
+(ricavi %>% 
+  filter(ANNO == 2021 & Costi== "Ricavo") %>% 
+  # filter(Classe %in% c("Prestazioni", "Vendite prodotti", "Ricavi da produzione")) %>%
+  rowwise() %>% 
+  mutate(TotRic = sum(TUff, TNonUff, na.rm = T)) %>% ungroup %>%   
+  filter(TotRic >0) %>% 
+  group_by(MESE,Dipartimento) %>%  
+  summarise(TRic= sum(TotRic, na.rm = TRUE))), by = c("Mese"="MESE","Dipartimento" )   
+
+) %>% 
+  mutate(Dipartimento = casefold(Dipartimento, upper = TRUE)) %>% 
+  
+left_join(
+    ftep , by= "Dipartimento") %>% 
+
+  mutate(RFTE = round((TRic/((FTET)*(FTp/100))), 2)) %>% View()
+  
+  
+ggplot()+
+  aes(x = Mese, 
+      y = RFTE)+
+  geom_point()+
+  geom_line()+
+  facet_wrap(~Dipartimento, ncol = 1, scales = "free_y")
 
 
-#ricavo totale mensile
-ricavi %>% 
-  filter(ANNO == 2021)
+#BISOGNA AGGIUNGERE IL DATO DEI FTE PROGRAMMATO....
+869939+
