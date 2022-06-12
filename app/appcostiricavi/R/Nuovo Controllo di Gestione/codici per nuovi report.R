d451f1e3a2b6a259a308b5da9923e41f674e0a34
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2022-06-13 00:12:55 +0200

    update

--- app/appcostiricavi/R/Nuovo Controllo di Gestione/codici per nuovi report.R
+++ app/appcostiricavi/R/Nuovo Controllo di Gestione/codici per nuovi report.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(here)
-
+library(gt)
 
 cc <- readRDS(here("data", "processed","CC.rds"))
 
@@ -59,8 +59,72 @@ R1 <- ricavi %>%
   rbind(costiIND)
 
 
+T <- R1 %>% select(- Categoria) %>% 
+  gt(rowname_col = "Classe")
 
+T %>%
+  cols_move_to_start(
+    columns =  8:10
+  ) %>% 
+  cols_move_to_end(
+    columns = 2
+  ) %>% 
+  tab_spanner(
+    label = "Direzione",
+    columns = c("Direzione Generale", "Direzione Sanitaria", "Direzione Amministrativa" )
+  ) %>% 
+  tab_spanner(
+    label = "Dipartimenti tecnici",
+    columns =  4:7
+  ) %>% 
+  
+  tab_row_group(
+    label = "Costi indiretti",
+    rows = 11:20) %>% 
+  tab_row_group(
+    label = "Costi diretti",
+    rows = 4:10) %>% 
+  tab_row_group(
+    label = "Proventi per attività",
+    rows = 1:3) %>% 
+  
+  summary_rows(
+    groups = "Proventi per attività",
+    formatter = fmt_currency,
+    currency = "EUR",
+    fns = list(
+    "Totale Valore della Produzione" = ~sum(., na.rm = TRUE))
+  ) %>% 
+  
+  summary_rows(
+    groups = "Costi diretti",
+    formatter = fmt_currency,
+    currency = "EUR",
+    fns = list(
+      "Totale Costi diretti" = ~sum(., na.rm = TRUE))
+  ) %>% 
 
+  summary_rows(
+    groups = "Costi indiretti",
+    formatter = fmt_currency,
+    currency = "EUR",
+    fns = list(
+      "Totale Costi indiretti" = ~sum(., na.rm = TRUE))
+  ) %>% 
+  
+  fmt_currency(
+    columns =  2:10,
+    currency = "EUR"
+  ) %>% 
+  
+  tab_options(
+    summary_row.background.color = "lightblue",
+    row_group.border.top.width = px(3),
+    row_group.border.top.color = "black",
+    row_group.border.bottom.color = "black"
+  ) 
+  
+  
 
 
 
