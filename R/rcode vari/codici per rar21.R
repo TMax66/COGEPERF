95a0e5fd8bd27ce333735df4716e619522769263
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2022-09-01 13:40:31 +0200

    update

--- R/rcode vari/codici per rar21.R
+++ R/rcode vari/codici per rar21.R
@@ -10,47 +10,76 @@ con <- DBI::dbConnect(odbc::odbc(), Driver = "SQL Server", Server = "dbprod02.iz
                       Database = "IZSLER", Port = 1433)
 #
 #
-query <- "SELECT
-dbo.Conferimenti.Numero,
-dbo_Anag_Reparti_ConfProp.Descrizione As StrProp,
-dbo_Anag_Reparti_ConfAcc.Descrizione As StrAcc,
-dbo.RDP_Date_Emissione.Istanza_RDP,
-dbo.Conferimenti.Data_Accettazione,
-dbo.RDP_Date_Emissione.Data_RDP
-FROM
-{ oj dbo.Anag_Reparti  dbo_Anag_Reparti_ConfProp INNER JOIN dbo.Laboratori_Reparto  dbo_Laboratori_Reparto_ConfProp ON ( dbo_Laboratori_Reparto_ConfProp.Reparto=dbo_Anag_Reparti_ConfProp.Codice )
-  INNER JOIN dbo.Conferimenti ON ( dbo.Conferimenti.RepLab=dbo_Laboratori_Reparto_ConfProp.Chiave )
-  INNER JOIN dbo.Anag_Tipo_Prel ON ( dbo.Conferimenti.Tipo_Prelievo=dbo.Anag_Tipo_Prel.Codice )
-  INNER JOIN dbo.Laboratori_Reparto  dbo_Laboratori_Reparto_ConfAcc ON ( dbo.Conferimenti.RepLab_Conferente=dbo_Laboratori_Reparto_ConfAcc.Chiave )
-  INNER JOIN dbo.Anag_Reparti  dbo_Anag_Reparti_ConfAcc ON ( dbo_Laboratori_Reparto_ConfAcc.Reparto=dbo_Anag_Reparti_ConfAcc.Codice )
-  LEFT OUTER JOIN dbo.RDP_Date_Emissione ON ( dbo.RDP_Date_Emissione.Anno=dbo.Conferimenti.Anno and dbo.RDP_Date_Emissione.Numero=dbo.Conferimenti.Numero )
-}
-WHERE
-(
-  {fn year(dbo.Conferimenti.Data_Accettazione)}  =  2021
-  AND  dbo.Anag_Tipo_Prel.Descrizione  =  'Ufficiale'
-)
-"
+# query <- "SELECT        Conferimenti.Numero, dbo_Anag_Reparti_ConfProp.Descrizione AS StrProp, dbo_Anag_Reparti_ConfAcc.Descrizione AS StrAcc, RDP_Date_Emissione.Istanza_RDP, Conferimenti.Data_Accettazione, 
+#                          RDP_Date_Emissione.Data_RDP, Conferimenti.NrCampioni
+# FROM            Anag_Reparti AS dbo_Anag_Reparti_ConfProp INNER JOIN
+#                          Laboratori_Reparto AS dbo_Laboratori_Reparto_ConfProp ON dbo_Laboratori_Reparto_ConfProp.Reparto = dbo_Anag_Reparti_ConfProp.Codice INNER JOIN
+#                          Conferimenti ON Conferimenti.RepLab = dbo_Laboratori_Reparto_ConfProp.Chiave INNER JOIN
+#                          Anag_Tipo_Prel ON Conferimenti.Tipo_Prelievo = Anag_Tipo_Prel.Codice INNER JOIN
+#                          Laboratori_Reparto AS dbo_Laboratori_Reparto_ConfAcc ON Conferimenti.RepLab_Conferente = dbo_Laboratori_Reparto_ConfAcc.Chiave INNER JOIN
+#                          Anag_Reparti AS dbo_Anag_Reparti_ConfAcc ON dbo_Laboratori_Reparto_ConfAcc.Reparto = dbo_Anag_Reparti_ConfAcc.Codice LEFT OUTER JOIN
+#                          RDP_Date_Emissione ON RDP_Date_Emissione.Anno = Conferimenti.Anno AND RDP_Date_Emissione.Numero = Conferimenti.Numero
+# WHERE        ({ fn YEAR(Conferimenti.Data_Accettazione) } = 2022) AND (Anag_Tipo_Prel.Descrizione = 'Ufficiale')
+# "
 
 
-rar21 <- con %>% tbl(sql(query)) %>% as_tibble()
+
+
+
+query <-"SELECT   dbo_Anag_Reparti_ConfProp.Descrizione  AS StrProp,
+  dbo_Anag_Reparti_ConfAcc.Descrizione AS StrAcc,
+  dbo.Conferimenti.Numero,
+  dbo.RDP_Date_Emissione.Istanza_RDP,
+  dbo.Conferimenti.Data_Accettazione,
+  dbo.RDP_Date_Emissione.Data_RDP,
+  dbo.Conferimenti.NrCampioni,
+  dbo_Anag_Finalita_Confer.Descrizione AS finalita,
+  dbo.Esami_Aggregati.Tot_Eseguiti
+
+FROM            Anag_Reparti AS dbo_Anag_Reparti_ConfProp INNER JOIN
+Laboratori_Reparto AS dbo_Laboratori_Reparto_ConfProp ON dbo_Laboratori_Reparto_ConfProp.Reparto = dbo_Anag_Reparti_ConfProp.Codice INNER JOIN
+Conferimenti ON Conferimenti.RepLab = dbo_Laboratori_Reparto_ConfProp.Chiave LEFT OUTER JOIN
+Esami_Aggregati ON Conferimenti.Anno = Esami_Aggregati.Anno_Conferimento AND Conferimenti.Numero = Esami_Aggregati.Numero_Conferimento INNER JOIN
+Anag_Tipo_Prel ON Conferimenti.Tipo_Prelievo = Anag_Tipo_Prel.Codice INNER JOIN
+Laboratori_Reparto AS dbo_Laboratori_Reparto_ConfAcc ON Conferimenti.RepLab_Conferente = dbo_Laboratori_Reparto_ConfAcc.Chiave INNER JOIN
+Anag_Reparti AS dbo_Anag_Reparti_ConfAcc ON dbo_Laboratori_Reparto_ConfAcc.Reparto = dbo_Anag_Reparti_ConfAcc.Codice INNER JOIN
+Conferimenti_Finalita ON Conferimenti.Anno = Conferimenti_Finalita.Anno AND Conferimenti.Numero = Conferimenti_Finalita.Numero INNER JOIN
+Anag_Finalita AS dbo_Anag_Finalita_Confer ON Conferimenti_Finalita.Finalita = dbo_Anag_Finalita_Confer.Codice LEFT OUTER JOIN
+RDP_Date_Emissione ON RDP_Date_Emissione.Anno = Conferimenti.Anno AND RDP_Date_Emissione.Numero = Conferimenti.Numero
+WHERE        (Esami_Aggregati.Esame_Altro_Ente = 0) AND (Esami_Aggregati.Esame_Altro_Ente = 0) AND ({ fn YEAR(Conferimenti.Data_Accettazione) } = 2022) AND (Anag_Tipo_Prel.Descrizione = 'Ufficiale')"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rar22 <- con %>% tbl(sql(query)) %>% as_tibble()
 
 
 
 
 #Importazione dati----
-rar21  <- read_delim("data/raw/datirar21.txt", 
-                                 "\t", escape_double = FALSE, col_names = FALSE, 
-                                 trim_ws = TRUE)
+# rar21  <- read_delim("data/raw/datirar21.txt", 
+#                                  "\t", escape_double = FALSE, col_names = FALSE, 
+#                                  trim_ws = TRUE)
 
-names(rar21) <- c("nconf","stran", "strpropr", "nrdp", "dtreg", "dtref")
-
-
+#names(rar22) <- c("nconf","stran", "strpropr", "nrdp", "dtreg", "dtref","")
 
 
 
-rar21 <- rar21 %>%
-  mutate(Struttura = recode(stran,
+
+
+rar22 <- rar22 %>%
+  mutate(Struttura = recode(StrAcc,
                             "S.T. PIACENZA E PARMA" = "SEDE TERRITORIALE DI PIACENZA - PARMA",
                             "REP. CHIM. DEGLI ALIMENTI E MANGIMI" = "REPARTO CHIMICA DEGLI ALIMENTI E MANGIMI",
                             "REP. CHIMICO ALIMENTI BOLOGNA" = "REPARTO CHIMICO DEGLI ALIMENTI (BOLOGNA)",
@@ -82,30 +111,34 @@ rar21 <- rar21 %>%
 
 
 ## analisi----
-
+library(openxlsx)
  
-tabella <- rar21 %>%
-filter(dtreg <= "2021-12-02" ) %>%  
-  group_by(stran) %>% 
+tabella <- rar22 %>%
+  filter(Data_Accettazione <= "2022-09-01") %>% 
+  group_by(StrAcc) %>% 
   summarise(n = n()) %>% 
   left_join(
-    
-    (rar21 %>% 
-       filter(dtreg <= "2021-12-02" ) %>%  
-       filter(nrdp != "NULL") %>% 
-    group_by(stran) %>% 
+    (rar22 %>% 
+       filter(!is.na(Istanza_RDP) ,
+              Data_Accettazione <= "2022-09-01") %>% 
+    group_by(StrAcc) %>% 
       summarise(n = n())  
-    ), by = "stran") %>%  
+    ), by = "StrAcc") %>%  
   mutate(
     "incorso" = n.x-n.y, 
     "%refertati" = round(100*(n.y/n.x),2)) %>%  as_tibble() %>% 
-  rename( "Struttura" = stran, "N.conferimenti ufficiali" =   n.x,  "N.conferimenti refertati" = n.y ,
-          "N.conferimenti con analisi in corso" = incorso , "% conferimenti refertati" = `%refertati`  ) 
+  rename( "Struttura" = StrAcc, "N.conferimenti ufficiali" =   n.x,  "N.conferimenti refertati" = n.y ,
+          "N.conferimenti con analisi in corso" = incorso , "% conferimenti refertati" = `%refertati`  ) %>% 
+ write.xlsx(file = "controllo attività.xlsx")
+  
   
 
 
 write.table(tabella, file = "tabella.txt")
 
-# print(xtable(tabella), type = "html", file = "tabella.html"
-# )      
-      
+
+rar22 %>% distinct() %>% 
+  filter(Data_Accettazione <= "2022-09-01") %>% 
+  group_by(StrAcc) %>% 
+  summarise(n = n()) %>% View()
+
