f9621591b006523a56106f7ff2bfa7bf58d19ff8
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2023-05-09 09:59:31 +0200

    update: modifica grafici RFTE

--- app/app_cogeperf/server.R
+++ app/app_cogeperf/server.R
@@ -892,33 +892,33 @@ output$tr <- renderUI({
   
   ##grafici RT e FTE----
   
-  p3<-reactive({dtmensili%>% 
-      filter(Dipartimento == input$dip & Anno == 2022) %>% 
-      ggplot()+
-      aes(x = MESE, y = RT)+
-      geom_point(alpha = 0.3)+
-      geom_line(group = 1, alpha = 0.3)+
-      geom_point(data = rfte23point(), aes(x=MESE, y=RT), color="red") +
-      geom_line(data = rfte23point(), aes(x=MESE, y=RT), color="red", lty=3) +
-      scale_x_discrete(limit = c(1,2,3,4,5,6,7,8,9,10,11,12))+
-      theme_bw()
-  })
-  
-  p4<-reactive({dtmensili%>% 
-      filter(Dipartimento == input$dip & Anno == 2022) %>% 
-      ggplot()+
-      aes(x = MESE, y = FTET*(FTp/100))+
-      labs(y = "FTET programmati per gli obiettivi valorizzati")+
-      geom_point(alpha = 0.3)+
-      geom_line(group = 1, alpha = 0.3)+
-      geom_point(data = rfte23point(), aes(x=MESE, y = FTET*(FTp/100)), color="red") +
-      geom_line(data = rfte23point(), aes(x=MESE, y = FTET*(FTp/100)), color="red", lty=3) +
-      scale_x_discrete(limit = c(1,2,3,4,5,6,7,8,9,10,11,12))+
-      theme_bw()
-  })
-  
-  P2 <-reactive({ p3()/p4()})
-  
+  # p3<-reactive({dtmensili%>% 
+  #     filter(Dipartimento == input$dip & Anno == 2022) %>% 
+  #     ggplot()+
+  #     aes(x = MESE, y = RT)+
+  #     geom_point(alpha = 0.3)+
+  #     geom_line(group = 1, alpha = 0.3)+
+  #     geom_point(data = rfte23point(), aes(x=MESE, y=RT), color="red") +
+  #     geom_line(data = rfte23point(), aes(x=MESE, y=RT), color="red", lty=3) +
+  #     scale_x_discrete(limit = c(1,2,3,4,5,6,7,8,9,10,11,12))+
+  #     theme_bw()
+  # })
+  # 
+  # p4<-reactive({dtmensili%>% 
+  #     filter(Dipartimento == input$dip & Anno == 2022) %>% 
+  #     ggplot()+
+  #     aes(x = MESE, y = FTET*(FTp/100))+
+  #     labs(y = "FTET programmati per gli obiettivi valorizzati")+
+  #     geom_point(alpha = 0.3)+
+  #     geom_line(group = 1, alpha = 0.3)+
+  #     geom_point(data = rfte23point(), aes(x=MESE, y = FTET*(FTp/100)), color="red") +
+  #     geom_line(data = rfte23point(), aes(x=MESE, y = FTET*(FTp/100)), color="red", lty=3) +
+  #     scale_x_discrete(limit = c(1,2,3,4,5,6,7,8,9,10,11,12))+
+  #     theme_bw()
+  # })
+  # 
+  # P2 <-reactive({ p3()/p4()})
+  # 
   
   
   
@@ -950,7 +950,7 @@ output$tr <- renderUI({
       aes(x = MESE, y = RFTE)+
       geom_point()+
       geom_line(group = 1, alpha = 0.3)+
-      geom_ribbon(aes(ymin = low, ymax = RFTE), fill = "grey70", alpha = 0.3) +
+      #geom_ribbon(aes(ymin = low, ymax = RFTE), fill = "grey70", alpha = 0.3) +
       geom_point(data = rfte23pointR(), aes(x=MESE, y=RFTE), color="red") +
       geom_line(data = rfte23pointR(), aes(x=MESE, y=RFTE), color="red", lty=3) +
       scale_x_discrete(limit = c(1,2,3,4,5,6,7,8,9,10,11,12))+
@@ -965,7 +965,7 @@ output$tr <- renderUI({
       aes(x = MESE, y = RFTEc)+
       geom_point()+
       geom_line(group = 1, alpha =0.5)+
-      geom_ribbon(aes(ymin = lowc, ymax = RFTEc), fill = "grey70", alpha = 0.3) +
+      #geom_ribbon(aes(ymin = lowc, ymax = RFTEc), fill = "grey70", alpha = 0.3) +
       geom_point(data = rfte23pointR(), aes(x=MESE, y=RFTEc), color="red") +
       geom_line(data = rfte23pointR(), aes(x=MESE, y=RFTEc), color="red", lty=3)+
       scale_x_discrete(limit = c(1,2,3,4,5,6,7,8,9,10,11,12))+
@@ -976,36 +976,36 @@ output$tr <- renderUI({
   
   ##grafici RT e FTE----
   
-  P5<-reactive({dtmensiliR%>% 
-      filter(Dipartimento == input$dip & Anno == 2022) %>% 
-      ggplot()+
-      aes(x = MESE, y = RT)+
-      geom_point(alpha = 0.3)+
-      geom_line(group = 1, alpha = 0.3)+
-      geom_point(data = rfte23pointR(), aes(x=MESE, y=RT), color="red") +
-      geom_line(data = rfte23pointR(), aes(x=MESE, y=RT), color="red", lty=3) +
-      scale_x_discrete(limit = c(1,2,3,4,5,6,7,8,9,10,11,12))+
-      facet_wrap(Reparto~., scales = "free")+
-      theme_bw()+
-      theme(strip.text = element_text(size = 6.5, color = "blue"))
-  })
-  
-  P6<-reactive({dtmensiliR%>% 
-      filter(Dipartimento == input$dip & Anno == 2022) %>% 
-      ggplot()+
-      aes(x = MESE, y = FTET*(FTp/100))+
-      labs(y = "FTET programmati per gli obiettivi valorizzati")+
-      geom_point(alpha = 0.3)+
-      geom_line(group = 1, alpha = 0.3)+
-      geom_point(data = rfte23pointR(), aes(x=MESE, y = FTET*(FTp/100)), color="red") +
-      geom_line(data = rfte23pointR(), aes(x=MESE, y = FTET*(FTp/100)), color="red", lty=3) +
-      scale_x_discrete(limit = c(1,2,3,4,5,6,7,8,9,10,11,12))+
-      facet_wrap(Reparto~., scales = "free")+
-      theme_bw()+
-      theme(strip.text = element_text(size = 6.5, color = "blue"))
-  })
-  
-  
+  # P5<-reactive({dtmensiliR%>% 
+  #     filter(Dipartimento == input$dip & Anno == 2022) %>% 
+  #     ggplot()+
+  #     aes(x = MESE, y = RT)+
+  #     geom_point(alpha = 0.3)+
+  #     geom_line(group = 1, alpha = 0.3)+
+  #     geom_point(data = rfte23pointR(), aes(x=MESE, y=RT), color="red") +
+  #     geom_line(data = rfte23pointR(), aes(x=MESE, y=RT), color="red", lty=3) +
+  #     scale_x_discrete(limit = c(1,2,3,4,5,6,7,8,9,10,11,12))+
+  #     facet_wrap(Reparto~., scales = "free")+
+  #     theme_bw()+
+  #     theme(strip.text = element_text(size = 6.5, color = "blue"))
+  # })
+  # 
+  # P6<-reactive({dtmensiliR%>% 
+  #     filter(Dipartimento == input$dip & Anno == 2022) %>% 
+  #     ggplot()+
+  #     aes(x = MESE, y = FTET*(FTp/100))+
+  #     labs(y = "FTET programmati per gli obiettivi valorizzati")+
+  #     geom_point(alpha = 0.3)+
+  #     geom_line(group = 1, alpha = 0.3)+
+  #     geom_point(data = rfte23pointR(), aes(x=MESE, y = FTET*(FTp/100)), color="red") +
+  #     geom_line(data = rfte23pointR(), aes(x=MESE, y = FTET*(FTp/100)), color="red", lty=3) +
+  #     scale_x_discrete(limit = c(1,2,3,4,5,6,7,8,9,10,11,12))+
+  #     facet_wrap(Reparto~., scales = "free")+
+  #     theme_bw()+
+  #     theme(strip.text = element_text(size = 6.5, color = "blue"))
+  # })
+  # 
+  # 
   
   
   # Codici per la produzione del report in Excel----
@@ -1053,23 +1053,22 @@ output$tr <- renderUI({
           "questo dato riportato in tabella e' calcolato moltiplicando i FTET per la % di FTp per le attivita' valorizzate", 
           "Ricavo Full Time Equivalente (RFTE)", 
           "RFTE e' un indicatore di performance che tiene conto dei volumi di attivita' valorizzata e delle risorse a disposizione per la loro realizzazione", 
-          "misurate come FTE. A partire dal 2022 l'RFTE viene assegnato a tutte le strutture che erogano attivita' analitiche come obiettivo di efficienza", 
-          "Target:raggiungimento di almeno il 90% del valore di RFTE al 31/12/2021", 
+          "misurate come FTE. Il  RFTE viene assegnato a tutti i Dipartimenti che erogano attivita' valorizzate ( attività analitiche, vendita prodotti,ecc) come obiettivo di efficienza", 
+          "Target: mantenimento del RFTE al 31/12/2022",
           "RFTE e' calcolato come RT/FTETp", 
+          "Il RFTE del 2022 è calcolato detraendo i ricavi dovuti all'attività COVID dal Ricavo Totale.",
+          " Tale detrazione non viene applicata per il 2023",
           "",
           "Monitoraggio", 
           "Per ogni dipartimento e per ogni struttura complessa o reparto", 
-          "viene calcolato il RFTE mensile e cumulato ottenuto nel corso del 2021", 
-          "Graficamente viene rappresentato sia l'andamento mensile che quello cumulato con l'aggiunta", 
-          "di una banda posta sotto la linea di andamento, la cui ampiezza corrisponde al valore del RFTE del 2021 a cui viene sottrato il 10%", 
-          "questa banda corrisponde all'area di raggiungimento del target previsto dall'obiettivo", 
-          "I dati dell'andamento del 2021 sono riportati in rosso e vengono aggiornati ad ogni trimestre in occasione dell'aggiornamento", 
-          "del data warehouse del controllo di gestione da parte dei Sistemi Informativi", 
-          "I punti relativi al RFTE del 2022 che cadono nella banda grigia o superiormente alla linea di andamento del RFTE del 2021,indicano", 
-          "il raggiungimento del target", 
-          "", 
-          "Alla rappresentazione grafica di cui sopra si aggiunge anche l'andamento mensile del RT e del FTE", 
-          "In questo modo sara' possibile valutare quale dei due parametri incide principalmente sul valore di RFTE ottenuto.", 
+          "viene calcolato il RFTE mensile e cumulato ottenuto nel corso del 2022", 
+          "Graficamente viene rappresentato sia l'andamento  cumulato che quello mensile", 
+        
+          "I dati dell'andamento del RFTE del 2023 sono riportati in rosso e vengono aggiornati ad ogni trimestre in occasione dell'aggiornamento", 
+          "del data warehouse del controllo di gestione da parte dei Sistemi Informativi",
+          "mentre l'andamento cumulato del 2022 che fa da riferimento è riportato in nero", 
+          
+  
           "", 
           "Ogni grafico e' accompagnato dalla tabella che riporta i dati utilizzati per la sua realizzazione",
           "I grafici sono inseriti nei diversi fogli del workbook come immagini e quindi non possono essere modificati"
@@ -1093,12 +1092,12 @@ output$tr <- renderUI({
       png("p11.png")
       print(P())
       dev.off()
-      png("p12.png")
-      print(P2())
-      dev.off()
+      # png("p12.png")
+      # print(P2())
+      # dev.off()
       
       insertImage(wb, "Dipartimento", "p11.png",  startCol = "J",  width = 5, height = 5)
-      insertImage(wb, "Dipartimento", "p12.png",  startCol = "Q",  width = 5, height = 5)
+     # insertImage(wb, "Dipartimento", "p12.png",  startCol = "Q",  width = 5, height = 5)
       
       #Reparti sheet ----
       
@@ -1119,26 +1118,26 @@ output$tr <- renderUI({
       print(P4())
       dev.off()
       
-      png("p15.png", width = 650)
-      print(P5())
-      dev.off()
-      
-      png("p16.png", width = 650)
-      print(P6())
-      dev.off()
+      # png("p15.png", width = 650)
+      # print(P5())
+      # dev.off()
+      # 
+      # png("p16.png", width = 650)
+      # print(P6())
+      # dev.off()
       
       
       insertImage(wb, "Reparti", "p13.png",  startCol = "K",width = 7, height = 5)
       insertImage(wb, "Reparti", "p14.png",  startCol = "T",width = 7, height = 5)
-      insertImage(wb, "Reparti", "p15.png",  startCol = "K",startRow = 27,width = 7, height = 5)
-      insertImage(wb, "Reparti", "p16.png",  startCol = "T",startRow = 27,width = 7, height = 5)
+      # insertImage(wb, "Reparti", "p15.png",  startCol = "K",startRow = 27,width = 7, height = 5)
+      # insertImage(wb, "Reparti", "p16.png",  startCol = "T",startRow = 27,width = 7, height = 5)
       
       
       
       
       saveWorkbook(wb, file, overwrite = T)
       
-      unlink(c(P(), P2(), P3(), P4(), P5(), P6()))
+      unlink(c(P(),  P3(), P4())) #, P5(), P6()))P2(),
       
     }
   )
